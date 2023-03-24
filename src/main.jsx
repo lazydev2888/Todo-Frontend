b97b00e16f9f25c8f4cf06a720dae8b66fe9c1e3
lazydevcoder <lazydevcoder@gmail.com>
2023-03-25 05:29:26 +0530

    Authentication Complete

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/App.scss'
+import {createContext} from 'react';
+
+export const server = 'https://node-todoapp.onrender.com/api/v1';
+
+
+export const Context = createContext({isAuthenticated: false});
+
+  const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return(
+      <Context.Provider value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        loading,
+        setLoading
+      }}>
+    <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <AppWrapper />  
+  </React.StrictMode>
+);
